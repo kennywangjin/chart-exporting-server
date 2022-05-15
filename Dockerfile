b8dd712b7b0bdff7731d737688ed0c9158ea1649
jw33002401 <kennywangjin@outlook.com>
2022-05-15 17:50:08 +0800

    refactor: 使用playwright替换puppeteer

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,16 @@
 FROM node:lts as builder
 ENV NODE_ENV build
-USER node
-WORKDIR /home/node
+WORKDIR /app
 COPY package*.json ./
-RUN npm ci
-COPY --chown=node:node . .
-RUN npm run build \
-    && npm prune --production
+RUN npm config set registry https://registry.npmmirror.com && npm install
+COPY . .
+RUN npm run build && npm prune --production
 
-# ---
-
-FROM node:lts
-RUN apt-get update \
-    && apt-get install -y wget gnupg \
-    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
-    && apt-get update \
-    && apt-get install -y google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 \
-    --no-install-recommends \
-    && rm -rf /var/lib/apt/lists/*
+FROM mcr.microsoft.com/playwright:v1.22.0-focal
 ENV NODE_ENV production
-USER node
 EXPOSE 3000
-WORKDIR /home/node
-COPY --from=builder --chown=node:node /home/node/package*.json ./
-COPY --from=builder --chown=node:node /home/node/node_modules/ ./node_modules/
-COPY --from=builder --chown=node:node /home/node/dist/ ./dist/
+WORKDIR /app
+COPY --from=builder /app/package*.json ./
+COPY --from=builder /app/node_modules/ ./node_modules/
+COPY --from=builder /app/dist/ ./dist/
 CMD ["node", "dist/main.js"]
--- src/echarts.service.ts
+++ src/echarts.service.ts
@@ -2,70 +2,68 @@ import { Injectable, Logger } from '@nestjs/common';
 import { chartOptions } from './chartOptions';
 import path = require('path');
 import echarts = require('echarts');
-import puppeteer = require('puppeteer');
+import playwright = require('playwright-core');
+
+interface RenderOptions extends playwright.JSHandle {
+  chartOptions: echarts.EChartsOption;
+  width: number;
+  height: number;
+}
 
 @Injectable()
 export class EChartsService {
   private readonly logger: Logger = new Logger(EChartsService.name);
+  private readonly pageHtml: string = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>ECharts</title>
+    </head>
+    <body>
+      <div id="chart"></div>
+    </body>
+    </html>`;
+
+  /**
+   * 根据配置、宽度、高度进行echarts图表渲染
+   * @param renderOptions 图表渲染配置项
+   */
+  renderChart(renderOptions: RenderOptions) {
+    const { chartOptions: options, width, height } = renderOptions;
+    const container = document.getElementById('chart');
+    container.style.width = `${width}px`;
+    container.style.height = `${height}px`;
+    const chart = echarts.init(container);
+    chart.setOption({ ...options, animation: false });
+  }
 
   async getChart(options: echarts.EChartsOption, outputOptions: chartOptions) {
-    this.logger.log('Starting puppeteer browser');
-    const browser = await puppeteer.launch({
+    this.logger.log('Starting chromium browser');
+    const browser = await playwright.chromium.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
     try {
-      this.logger.debug('Starting puppeteer page');
+      this.logger.debug('Starting new page');
       const page = await browser.newPage();
-      await page.setContent(
-        `<!DOCTYPE html>
-          <html lang="en">
-            <head>
-              <meta charset="UTF-8">
-              <meta http-equiv="X-UA-Compatible" content="IE=edge">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <title>ECharts</title>
-            </head>
-            <body>
-              <div id="chart"></div>
-            </body>
-            </html>`,
-      );
+      await page.setContent(this.pageHtml);
       await page.addScriptTag({
         path: path.join(__dirname, '../node_modules/echarts/dist/echarts.js'),
       });
 
-      this.logger.debug('Evaluating echarts script');
-      await page.evaluate(
-        renderChart,
-        options as puppeteer.JSONObject,
-        outputOptions.width,
-        outputOptions.height,
-      );
+      this.logger.debug('Generating chart image');
+      await page.evaluate(this.renderChart, {
+        chartOptions: options,
+        ...outputOptions,
+      } as RenderOptions);
       const element = await page.$('#chart');
-      this.logger.debug('Exporting chart');
+      this.logger.debug('Exporting chart image');
       return await element.screenshot({ type: 'png' });
     } finally {
-      this.logger.log('Closing puppeteer');
+      this.logger.log('Closing browser');
       await browser.close();
     }
   }
-}
-
-/**
- * 根据配置、宽度、高度进行echarts图表渲染
- * @param options 图表配置项
- * @param width 图表宽度
- * @param height 图表高度
- */
-function renderChart(
-  options: echarts.EChartsOption,
-  width: number,
-  height: number,
-) {
-  const container = document.getElementById('chart');
-  container.style.width = `${width}px`;
-  container.style.height = `${height}px`;
-  const chart = echarts.init(container);
-  chart.setOption({ ...options, animation: false });
 }
