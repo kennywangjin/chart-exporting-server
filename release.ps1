08a60bb6f185dc74ee2ae4606c42cfced770ae97
王锦 <kennywangjin@outlook.com>
2023-03-06 14:19:34 +0800

    [feat]: 添加highcharts支持

--- release.ps1
+++ release.ps1
@@ -1,6 +1,6 @@
 Write-Host "Start building docker image"
 
-$ImageVersion = "0.0.3.0"
+$ImageVersion = "0.0.4.0"
 $ImageTag = "mfcharbor.mflex.com.cn/mes/echarts-exporting-server"
 
 Start-Process "docker" `
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EChartsController } from './echarts.controller';
 import { EChartsService } from './echarts.service';
+import { HighchartsController } from './highcharts.controller';
+import { HighchartsService } from './highcharts.service';
 import { PdfController } from './pdf.controller';
 import { PdfService } from './pdf.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, EChartsController, PdfController],
-  providers: [AppService, EChartsService, PdfService],
+  controllers: [
+    AppController,
+    EChartsController,
+    PdfController,
+    HighchartsController,
+  ],
+  providers: [AppService, EChartsService, PdfService, HighchartsService],
 })
 export class AppModule {}
